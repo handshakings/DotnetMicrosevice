3322f261e246f87398feaa36681936c9eb5869bf
handshakings <handshakings@gmail.com>
2022-01-17 14:16:02 +0500

    Created APIs, UserRepository and Injected dependency in Program.cs

--- UserService/Controllers/UsersController.cs
+++ UserService/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Database.Entities;
+using UserService.Repository;
 
 namespace UserService.Controllers
 {
@@ -6,7 +8,33 @@ namespace UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        
+        private readonly IUserRepository userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => StatusCode(StatusCodes.Status200OK, await userRepository.GetAll());
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id) => StatusCode(StatusCodes.Status200OK, await userRepository.GetUser(id));
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] User user)
+        {
+            bool isAdded = await userRepository.AddUser(user);
+            if (isAdded)
+            {
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
 
     }
 }
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,6 +1,6 @@
-
 using Microsoft.EntityFrameworkCore;
 using UserService.Database;
+using UserService.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 string conString = builder.Configuration.GetSection("ConnectionString").Value;
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(conString));
 builder.Services.AddControllers();
-
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,7 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.Run(async context => await context.Response.WriteAsync("Returning response from Program.cs inline middleware"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
